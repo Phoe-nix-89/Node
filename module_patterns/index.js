ce4dab94426cabce1e2f94b0c513ba69e7bf03ed
Phoe-nix-89 <sonkhoseth074@gmail.com>
2024-07-17 18:10:56 +0530

    Module patterns

---
+++ module_patterns/index.js
@@ -0,0 +1,6 @@
+const s = require('./searching.js')
+let arr = [5,4,3,2,1,7,9,0];
+let x = 1;
+
+console.log(s.linear(arr,x));
+console.log(s);
---
+++ module_patterns/searching.js
@@ -0,0 +1,17 @@
+let LS = (arr,x) => {
+    let idx = -1;
+    arr.forEach((elem,index) => {
+        if(elem == x) idx = index;
+    })
+
+    return idx;
+}
+
+let BS = () => {
+    // the code for binary search
+}
+
+module.exports = {
+    linear : LS,
+    binary : BS
+}
